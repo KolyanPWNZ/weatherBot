50b062381787eddd5b814f827aa9bcba3d497a25
Nikolay <krukolnik@gmail.com>
2022-03-26 19:39:38 +0300

    Добавил поиск погоды по названию городаэ

--- __init__.py
+++ __init__.py
@@ -12,17 +12,37 @@ def start(message):
     welcome_message = 'Привет! Это бот для определения погоды.'
     menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
     menu_item_location = types.KeyboardButton('Узнать погоду по геолокации', request_location=True)
-    menu.add(menu_item_location)
+    menu_item_city = types.KeyboardButton('Узнать погоду по названию города', )
+    menu.add(menu_item_location, menu_item_city) #
     bot.send_message(message.chat.id, welcome_message, reply_markup=menu)
 
 @bot.message_handler(content_types=['location'])
-def get_weather(message):
+def get_weather_by_coord(message):
     latitude = message.location.latitude
     longitude = message.location.longitude
     units = 'metric'
     language = 'ru'
-    city, desc, temp = weather_map.get(longitude, latitude, units, language)
-    data = f'Погода по вашему местоположению:\nГород: {city}\nТемпература: {temp} °C\n{desc}'
-    bot.send_message(message.chat.id, data)
+    city, desc, temp = weather_map.get_by_coord(longitude, latitude, units, language)
+    print(type(message.chat.id))
+    print_weather(message.chat.id, city, desc, temp)
+
+
+# вывод сообщения о погоде
+def print_weather(chat_id: int, city: str, desc: str, temp: int):
+    data = f'Полученные данные по погоде:\nГород: {city}\nТемпература: {temp} °C\n{desc}'
+    bot.send_message(chat_id, data)
+
+@bot.message_handler(content_types=['text'])
+def request_city_name(message):
+    send = bot.send_message(message.chat.id, "Введите название города:")
+    bot.register_next_step_handler(send, get_weather_by_name, get_weather_by_name)
+
+
+def get_weather_by_name(message, info):
+    city = message.text
+    language = message.from_user.language_code
+    desc, temp = weather_map.get_by_name(city, language)
+    print_weather(message.chat.id, city, desc, temp)
+
 
 bot.polling(none_stop=True)
--- openweathermap.py
+++ openweathermap.py
@@ -1,18 +1,24 @@
 import requests
 import config
+from pyowm import OWM
+from pyowm.utils.config import get_default_config
+
+
+
 
 
 class OpenWeatherMap:
     __base_url = 'https://api.openweathermap.org/data/2.5/weather'
 
+
     @property
     def base_url(self):
         return OpenWeatherMap.__base_url
 
+    # получение погоды по координатам
     @staticmethod
-    def get(longitute: float, latitude: float, units: str, language: str):
+    def get_by_coord(longitute: float, latitude: float, units: str, language: str):
         try:
-
             data = requests.get(
                 OpenWeatherMap.__base_url,
                 params={
@@ -22,12 +28,28 @@ class OpenWeatherMap:
                     'lang': language,
                     'appid': config.weather_token
                 }).json()
-
             city = data['name']
             description = data['weather'][0]['description']
             temp = data['main']['temp']
-            return city, description, str(temp)
+            return city, description, round(temp)
+
         except Exception as err:
             return err
 
+    # получение погоды по названию города
+    @staticmethod
+    def get_by_name(city: str, language: str):
+        try:
+            config_dict = get_default_config()
+            config_dict['language'] = language
 
+            owm = OWM(config.weather_token, config_dict)
+            mgr = owm.weather_manager()
+            observation = mgr.weather_at_place(city)
+            weather = observation.weather
+            temp = weather.temperature('celsius')['temp']
+            description = weather.detailed_status
+            return description, round(temp)
+
+        except Exception as err:
+            return err
